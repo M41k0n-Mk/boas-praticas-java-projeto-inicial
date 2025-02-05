2baff7ef77cc4ce5c5bd481aba21962728c26ded
M41k0n <142348031+M41k0n-Mk@users.noreply.github.com>
2025-02-05 02:46:35 -0300

    refact: extrai lógica do controller para dentro da classe AdocaoService do tipo service

--- src/main/java/br/com/alura/adopet/api/controller/AdocaoController.java
+++ src/main/java/br/com/alura/adopet/api/controller/AdocaoController.java
@@ -4,6 +4,7 @@ import br.com.alura.adopet.api.model.Adocao;
 import br.com.alura.adopet.api.model.StatusAdocao;
 import br.com.alura.adopet.api.service.AdocaoService;
 import jakarta.validation.Valid;
+import jakarta.validation.ValidationException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.mail.SimpleMailMessage;
@@ -15,47 +16,30 @@ import java.time.format.DateTimeFormatter;
 @RestController
 @RequestMapping("/adocoes")
 public class AdocaoController {
-
     @Autowired
     AdocaoService adocaoService;
 
     @PostMapping
     @Transactional
     public ResponseEntity<String> solicitar(@RequestBody @Valid Adocao adocao) {
-        this.adocaoService.solicitar(adocao);
-
+        try {
+            this.adocaoService.solicitar(adocao);
+            return ResponseEntity.ok("Adocao solicitada com sucesso!");
+        } catch (ValidationException e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
     }
 
     @PutMapping("/aprovar")
     @Transactional
     public ResponseEntity<String> aprovar(@RequestBody @Valid Adocao adocao) {
-        adocao.setStatus(StatusAdocao.APROVADO);
-        repository.save(adocao);
-
-        SimpleMailMessage email = new SimpleMailMessage();
-        email.setFrom("adopet@email.com.br");
-        email.setTo(adocao.getTutor().getEmail());
-        email.setSubject("Adoção aprovada");
-        email.setText("Parabéns " + adocao.getTutor().getNome() + "!\n\nSua adoção do pet " + adocao.getPet().getNome() + ", solicitada em " + adocao.getData().format(DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss")) + ", foi aprovada.\nFavor entrar em contato com o abrigo " + adocao.getPet().getAbrigo().getNome() + " para agendar a busca do seu pet.");
-        emailSender.send(email);
-
-        return ResponseEntity.ok().build();
+        this.adocaoService.aprovar(adocao);
     }
 
     @PutMapping("/reprovar")
     @Transactional
     public ResponseEntity<String> reprovar(@RequestBody @Valid Adocao adocao) {
-        adocao.setStatus(StatusAdocao.REPROVADO);
-        repository.save(adocao);
-
-        SimpleMailMessage email = new SimpleMailMessage();
-        email.setFrom("adopet@email.com.br");
-        email.setTo(adocao.getTutor().getEmail());
-        email.setSubject("Adoção reprovada");
-        email.setText("Olá " + adocao.getTutor().getNome() + "!\n\nInfelizmente sua adoção do pet " + adocao.getPet().getNome() + ", solicitada em " + adocao.getData().format(DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss")) + ", foi reprovada pelo abrigo " + adocao.getPet().getAbrigo().getNome() + " com a seguinte justificativa: " + adocao.getJustificativaStatus());
-        emailSender.send(email);
-
-        return ResponseEntity.ok().build();
+        this.adocaoService.reprovar(adocao);
     }
 
 }
